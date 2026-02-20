1274b33c2cf8f270b4aa32451fb0df03f3d13147
Soudip Panja <soudip03panja@gmail.com>
2026-02-20 15:20:25 +0530

    fix: saperated the route page and index page

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
+
+const authRoute = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const SECRET_KEY = "soudip12345";
-const JWT_SECRET = "my_jwt_secret";
-
-const verifyJwt = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(401).json({ message: "No token provided." });
-  }
-  try {
-    const decodedToken = jwt.verify(token, JWT_SECRET);
-    req.users = decodedToken;
-    next();
-  } catch (error) {
-    return res.status(402).json({ message: "Invalid Token" });
-  }
-};
-
-app.post("/login", (req, res) => {
-  const { secret } = req.body;
-
-  if (secret === SECRET_KEY) {
-    const token = jwt.sign({ role: "admin" }, JWT_SECRET, { expiresIn: "24h" });
-    res.json({ token: token });
-  } else {
-    res.json({ message: "Access Denied" });
-  }
-});
-
-app.get("/login/admin", verifyJwt, (req, res) => {
-  res.json({ message: "Protected route accessable." });
-});
+app.use("/", authRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+const SECRET_KEY = "soudip12345";
+const JWT_SECRET = "my_jwt_secret";
+
+// Middleware
+const verifyJwt = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
+  }
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.users = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(402).json({ message: "Invalid Token" });
+  }
+};
+
+// Logic Route
+router.post("/login", (req, res) => {
+  const { secret } = req.body;
+
+  if (secret === SECRET_KEY) {
+    const token = jwt.sign({ role: "admin" }, JWT_SECRET, { expiresIn: "24h" });
+    res.json({ token: token });
+  } else {
+    res.json({ message: "Access Denied" });
+  }
+});
+
+// Protected Route
+router.get("/login/admin", verifyJwt, (req, res) => {
+  res.json({ message: "Protected route accessable." });
+});
+
+module.exports = router;

6ac795ce5ed85c1d2deb3bff9d067a8696cc1cdd
Soudip Panja <soudip03panja@gmail.com>
2026-02-23 11:04:16 +0530

    feat: team model added

---
+++ models/team.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    unique: true,
+  }, // Team name must be unique
+  description: {
+    type: String,
+  }, //Optional description for the team
+});
+
+module.exports = mongoose.model("Team", teamSchema);

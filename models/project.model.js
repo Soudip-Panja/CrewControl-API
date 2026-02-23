52e24f5a8c501834cfc24ca23bbf6bca986fc55d
Soudip Panja <soudip03panja@gmail.com>
2026-02-23 11:26:22 +0530

    feat: project model added

---
+++ models/project.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    unique: true,
+  }, //Project name must be unique
+  description: {
+    type: String,
+  }, //Optional field for project description
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model('Project', projectSchema);

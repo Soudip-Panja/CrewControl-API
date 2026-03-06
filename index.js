1011cfac119a286992f3deba3413ebf63fef9200
Soudip Panja <soudip03panja@gmail.com>
2026-03-06 19:18:22 +0530

    test: tested create new user function

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ app.use(cors());
 
 app.use("/", authRoute);
 
+
+const { createUser } = require("./routes/users");
+
+const newUser = {
+  name: "Radha Panja",
+  email: "radha03pan@gmail.com",
+  password: "radha123",
+};
+
+createUser(newUser);
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const User = require("../models/user.model");
+
+const app = express();
+
+app.use(express.json());
+
+
+
+//Creat a new user
+async function createUser(newUser) {
+  try {
+    const user = new User(newUser);
+    const saveUser = await user.save();
+    console.log("New user:", saveUser)
+  } catch (error) {
+    console.log("Error creating new user", error);
+  }
+}
+module.exports = { createUser };

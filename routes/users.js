3a5770a471e6805f70bef5e999d9f2e8824fe3de
Soudip Panja <soudip03panja@gmail.com>
2026-03-06 20:09:30 +0530

    feat: validation added in create new user

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,17 @@ const app = express();
 
 app.use(express.json());
 
-
-
 //Creat a new user
 async function createUser(newUser) {
   try {
-    const user = new User(newUser);
-    const saveUser = await user.save();
-    console.log("New user:", saveUser)
+    if (!newUser.name || !newUser.email || !newUser.password) {
+      console.log("Name, Email and Password are require.");
+      return;
+    } else {
+      const user = new User(newUser);
+      const saveUser = await user.save();
+      console.log("New user:", saveUser);
+    }
   } catch (error) {
     console.log("Error creating new user", error);
   }
